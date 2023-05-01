e946f1d4fd2d5e7d7798727d1c19ebb5f94909e8
weejihye <kikiwjh@naver.com>
2023-05-01 13:30:40 +0900

    [fix] single search 에서 init()하는 부분 삭제

--- src/service/restaurant.service.ts
+++ src/service/restaurant.service.ts
@@ -3,9 +3,6 @@ import { pool, init } from '../db/pg-pool'
 
 
 export const getRestaurantById = async (id: number) => {
-
-  const client = await pool.connect();
-
   init();
   try { 
     const result = await findRestaurantById(id);
