2a09181df686c20db0ea582477f4632995caee19
weejihye <kikiwjh@naver.com>
2023-06-02 12:18:05 +0900

    [feat] supabase db import, config파일 생성 및 api 세팅 필요 #6

--- src/db/query/interface/restaurant.repository.ts
+++ src/db/query/interface/restaurant.repository.ts
@@ -1,5 +1,29 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://hqwnwxqptxpzbqrmtasy.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 export interface RestaurantRepository {
   findRestaurantById(id: number): Promise<any>;
   findAllRestaurants(): Promise<any[]>;
   findAllRestaurantsByCluster(cluster: string): Promise<any[]>;
 }
+
+export class RestaurantRepositoryMock implements RestaurantRepository {
+  constructor() {}
+
+  public async findRestaurantById(id: number) {
+    return restaurants.find((restaurant) => restaurant.id === id);
+  }
+
+  public async findAllRestaurants() {
+    return restaurants;
+  }
+
+  public async findAllRestaurantsByCluster(cluster: string) {
+    return restaurants.filter((restaurant) => restaurant.cluster === cluster);
+  }
+}
+
+const restaurants = data;
--- src/service/restaurant.service.test.ts
+++ src/service/restaurant.service.test.ts
@@ -1,17 +1,65 @@
-import { describe, test } from 'vitest';
+import { describe, test, expect, assert } from 'vitest';
+import { getAllRestaurants, getRestaurantById } from './restaurant.service';
 
 describe('단건 조회', () => {
-  test.todo('id가 0일 때, 이름이 맥도날드 서초뱅뱅점이어야함.');
+  test('id가 0일 때, 이름이 맥도날드 서초뱅뱅점이어야함.', async() => {
+    const id = 0;
+    const result = await getRestaurantById(id);
+  
+    expect(result.name).toBe('맥도날드 서초뱅뱅');
+  });
+  
+  test.todo('단건조회를 했을 때, 모든 정보를 포함해야함.', async() => {
+    const id = 0;
+    const result = await getRestaurantById(id);
 
-  test.todo('단건조회를 했을 때, 모든 정보를 포함해야함.');
+    expect(result).toEqual({
+      id: 0,
+      name: expect.any(String),
+      price: expect.any(String),
+      category: expect.any(String),
+      summary: expect.any(String),
+      cluster: expect.any(String),
+      latitude: expect.any(Number),
+      longitude: expect.any(Number),
+      detail: expect.any(String),
+    });
+  });
 
-  test.todo('찾는 id가 없을 때 Error를 던져야함.');
+  test.todo('찾는 id가 없을 때 Error를 던져야함.', async() => {
+    const id = 37;
+    const action = () => getRestaurantById(id);
+
+    expect(action).toThrow();
+  });
 });
 
+/*
+describe('메뉴 조회', () => {
+  test('맥도날드 메뉴는 10개 이상이어야함.', async () => {
+    const restaurant = await getRestaurantById(0);
+    assert.lengthOf(restaurant?.menu, 10);
+  });
+*/
+
 describe('전체 조회', () => {
-  test.todo('전체 조회 시, 전체 식당이 조회되어야함.');
+  test.todo('전체 조회 시, 전체 식당이 조회되어야함.', async() => {
+    const result = await getAllRestaurants();
 
-  test.todo('전체 조회 시, id, name, price, category, summary, cluster만 조회되어야함.');
+    assert.lengthOf(result, 37);
+  });
 
-  test.todo('전체 조회 시, cluster이 있으면 해당 cluster의 식당만 조회되어야함.');
+  test.todo('전체 조회 시, id, name, price, category, summary, cluster만 조회되어야함.', async() => {
+    const result = await getAllRestaurants();
+    expect(result[0]).not.toHaveProperty('latitude');
+    expect(result[0]).not.toHaveProperty('longitude');
+    expect(result[0]).not.toHaveProperty('detail');
+  });
+
+  test.todo('전체 조회 시, cluster이 있으면 해당 cluster의 식당만 조회되어야함.', async() => {
+    const cluster = 'seocho';
+    const result = await getAllRestaurants('seocho');
+
+    assert.lengthOf(result, 35);
+  });
 });
