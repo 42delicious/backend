83bd324cb73641dcb28113889d8f044bc1c06ac1
Junsang Mun <mun.js@aol.com>
2023-06-07 14:35:07 +0900

    [feat] add pg client

--- src/db/query/interface/restaurant.repository.ts
+++ src/db/query/interface/restaurant.repository.ts
@@ -1,6 +1,15 @@
+import { Client } from 'pg';
 import dotenv from 'dotenv'
 
 dotenv.config();
+
+const client = new Client({
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  database: process.env.DB_NAME || 'postgres',
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'postgres',
+});
 
 export interface RestaurantRepository {
   findRestaurantById(id: number): Promise<any>;
